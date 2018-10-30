1c74f16bd9b35750268f5f2c8be3e1b42ae048a9
Andrew T. Carl <atcarl@gmail.com>
2018-10-30 14:12:57 -0400

    Add files via upload

--- JOINS and Subqueries Contd.sql
+++ JOINS and Subqueries Contd.sql
@@ -35,3 +35,17 @@ INNER JOIN regions regions r
 ON e.region_id = r.region_id
 WHERE hire_date = (SELECT MAX(hire_date) FROM employees e2
 
+SELECT first_name, hire_date, hire_date - 90
+FROM employees
+
+SELECT first_name, hire_date, hire_date + 90
+FROM employees
+WHERE hire_date BETWEEN hire_date AND hire_date - 90
+
+SELECT hire_date, salary,
+(SELECT SUM(salary) FROM employees e2
+WHERE e2.hire_date BETWEEN e.hire_date - 90 AND e.hire_date) as spending pattern
+FROM employees e
+ORDER BY hire_date
+# Returns Hire date, salary and spending pattern.
+
